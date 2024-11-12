cc447ae11f456f9e44922af0a26e1a007a0a6378
Siyuan Jing <crazycyy233@gmail.com>
2024-11-11 20:29:19 -0500

    Update ReadImage.cpp

--- ReadImage.cpp
+++ ReadImage.cpp
@@ -1,1 +1,49 @@
+#include <opencv2/opencv.hpp>
+#include <vector>
 
+using namespace cv;
+using namespace std;
+
+struct PixelFeature {
+    Vec3b colorRGB;    // RGB 颜色值
+    Point position;    // 像素位置
+    float gradient;    // 梯度幅度
+    float intensity;   // 灰度值
+};
+
+vector<vector<PixelFeature>> extractFeatures(const Mat& image) {
+    // 转换图像为灰度图和梯度图
+    Mat gray, gradX, gradY, gradMag;
+    cvtColor(image, gray, COLOR_BGR2GRAY);
+    Sobel(gray, gradX, CV_32F, 1, 0); // X 方向梯度
+    Sobel(gray, gradY, CV_32F, 0, 1); // Y 方向梯度
+    magnitude(gradX, gradY, gradMag); // 梯度幅度
+
+    vector<vector<PixelFeature>> features(image.rows, vector<PixelFeature>(image.cols));
+
+    for (int y = 0; y < image.rows; ++y) {
+        for (int x = 0; x < image.cols; ++x) {
+            PixelFeature pf;
+            pf.colorRGB = image.at<Vec3b>(y, x);     // 提取颜色
+            pf.position = Point(x, y);               // 提取位置
+            pf.gradient = gradMag.at<float>(y, x);   // 提取梯度
+            pf.intensity = gray.at<uchar>(y, x);     // 提取亮度
+
+            features[y][x] = pf;
+        }
+    }
+
+    return features;
+}
+
+int main() {
+    Mat image = imread(".jpg");
+    if (image.empty()) {
+        cout << "Unable to read picture" << endl;
+        return -1;
+    }
+
+    auto features = extractFeatures(image);
+
+    // 后续代码：将特征输入图分割或最大流算法进行分割
+}
