20655e7e13c0a41521f96d0612cc29b40865bdf8
Siyuan Jing <crazycyy233@gmail.com>
2024-11-11 20:34:41 -0500

    Update FeatureExtractor.h

--- FeatureExtractor.h
+++ FeatureExtractor.h
@@ -1,4 +1,3 @@
-// FeatureExtractor.h
 #ifndef FEATURE_EXTRACTOR_H
 #define FEATURE_EXTRACTOR_H
 
@@ -6,13 +5,12 @@
 #include <vector>
 
 struct PixelFeature {
-    cv::Vec3b colorRGB;    // RGB 颜色值
-    cv::Point position;    // 像素位置
-    float gradient;        // 梯度幅度
-    float intensity;       // 灰度值
+    cv::Vec3b colorRGB;    // RGB colour values
+    cv::Point position;    // image element position
+    float gradient;        // gradient range
+    float intensity;       // greyscale value
 };
 
-// 提取图像特征的函数声明
 std::vector<std::vector<PixelFeature>> extractFeatures(const cv::Mat& image);
 
 #endif // FEATURE_EXTRACTOR_H
