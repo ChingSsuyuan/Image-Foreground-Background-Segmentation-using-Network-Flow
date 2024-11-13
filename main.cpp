d7dc1b58c465309e29aa1ec63a4009b013e49b99
Siyuan Jing <crazycyy233@gmail.com>
2024-11-13 10:11:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,27 +7,22 @@ using namespace cv;
 using namespace std;
 
 int main() {
-    // 加载输入图像
     Mat image = imread("input.jpg");
     if (image.empty()) {
         cout << "Fail to read Image" << endl;
         return -1;
     }
 
-    // 提取图像特征
     auto features = extractFeatures(image);
 
-    // 创建BST对象
     BST bst;
 
-    // 将特征数据存入BST
     for (int y = 0; y < image.rows; ++y) {
         for (int x = 0; x < image.cols; ++x) {
             bst.insert(features[y][x]);
         }
     }
 
-    // 遍历BST，输出特征数据
     cout << "BST data：" << endl;
     bst.inorderTraversal();
 
