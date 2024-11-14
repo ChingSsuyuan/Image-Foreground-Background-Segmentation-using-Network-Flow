6eeb84fd67f42f794f5cb272ffb23d3202d4625c
Siyuan Jing <crazycyy233@gmail.com>
2024-11-14 16:19:59 -0500

    Create Construct_Graph.h

---
+++ Construct_Graph.h
@@ -0,0 +1,46 @@
+#ifndef CONSTRUCT_GRAPH_H
+#define CONSTRUCT_GRAPH_H
+
+#include <opencv2/opencv.hpp>
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+// PixelFeature 结构，用于表示像素特征
+struct PixelFeature {
+    cv::Point position;       // 像素位置
+    cv::Vec3b colorRGB;       // 像素的 RGB 值
+    double gradientMagnitude; // 梯度幅值
+
+    PixelFeature(int x, int y, const cv::Vec3b& color, double gradient)
+        : position(x, y), colorRGB(color), gradientMagnitude(gradient) {}
+};
+
+// PairHash，用于为 std::pair<int, int> 定义哈希
+struct PairHash {
+    template <class T1, class T2>
+    std::size_t operator()(const std::pair<T1, T2>& p) const {
+        return std::hash<T1>()(p.first) ^ std::hash<T2>()(p.second);
+    }
+};
+
+// ListNode 结构，用于表示邻接链表的节点
+struct ListNode {
+    int x;                      // 节点 x 坐标
+    int y;                      // 节点 y 坐标
+    int weight;                 // 到相邻点的权重
+    std::shared_ptr<ListNode> next; // 指向链表下一个节点
+
+    ListNode(int x_, int y_, int w) : x(x_), y(y_), weight(w), next(nullptr) {}
+};
+
+// AdjacencyList 类型，使用 unordered_map 存储邻接表
+using AdjacencyList = std::unordered_map<std::pair<int, int>, std::shared_ptr<ListNode>, PairHash>;
+
+// 计算特征之间相似度权重的函数
+int calculateSimilarityWeight(const PixelFeature& feature1, const PixelFeature& feature2);
+
+// 生成整个图的邻接表
+AdjacencyList generateGraph(cv::Mat& image, const std::vector<std::vector<PixelFeature>>& features);
+
+#endif // CONSTRUCT_GRAPH_H
